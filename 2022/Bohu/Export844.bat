71fba2140d81a0b0b20a64cd7a5f5d693ecec40a
JachyHm <jachym.hurtik@gmail.com>
2023-09-02 05:10:13 +0200

    Export vozidla

--- 2022/Bohu/Export844.bat
+++ 2022/Bohu/Export844.bat
@@ -1,2 +1,2 @@
-CleanUp.exe "BohuRegio.xml" -b=5 -a=1 -z -d -r=unipis:mp3 -v="Out/844/unipis.xml"
+CleanUp.exe "BohuRegio_vylukaKuLOso.xml" -b=5 -a=1 -z -d -r=unipis:mp3 -v="Out/844/unipis.xml"
 Packer.exe "Out/844/unipis.xml" "Out/844"
