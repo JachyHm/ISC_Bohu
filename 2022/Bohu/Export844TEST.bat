d99ef3a0f5fe42e142e1505cd92fe9a85c8f2865
JachyHm <jachym.hurtik@gmail.com>
2023-09-21 20:45:13 +0200

    Export den ZK

--- 2022/Bohu/Export844TEST.bat
+++ 2022/Bohu/Export844TEST.bat
@@ -1,2 +1,2 @@
-CleanUp.exe "BohuRegio_vylukaKuLOso.xml" -b=5 -a=1 -r=unipis:mp3 -v="Out/844TEST/unipis.xml"
+CleanUp.exe "BohuRegio.xml" -b=5 -a=1 -r=unipis:mp3 -v="Out/844TEST/unipis.xml"
 Packer.exe "Out/844TEST/unipis.xml" "Out/844TEST"
